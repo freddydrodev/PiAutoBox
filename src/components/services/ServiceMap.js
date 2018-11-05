e3ab2ad7131e3c9eebb1c29702fc785f5b70fbd7
Fredius Tout Court <frediustcdev@gmail.com>
2018-11-05 21:31:56 +0000

    some packages added

--- src/components/services/ServiceMap.js
+++ src/components/services/ServiceMap.js
@@ -2,8 +2,14 @@ import React from "react";
 import { MapView } from "expo";
 import mapStyle from "../../config/mapStyle.json";
 import { rnFill } from "../../tools/utils.js";
+import MapViewDirections from "../../tools/MapViewDirections.js";
+
+const origin = { latitude: 5.5771556, longitude: -0.2574945 };
+const destination = { latitude: 5.6771556, longitude: -0.3574945 };
+const GOOGLE_MAPS_APIKEY = "AIzaSyDIYekVGFZ7z7llooI3CgzB9oCNo5ROBtA";
 
 const ServiceMap = ({ location, markers }) => {
+  console.log(location);
   return (
     <MapView
       style={{ flex: 1 }}
@@ -16,7 +22,14 @@ const ServiceMap = ({ location, markers }) => {
         longitudeDelta: 0.006
       }}
       customMapStyle={mapStyle}
-    />
+      loadingEnabled={true}
+    >
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+      />
+    </MapView>
   );
 };
 
